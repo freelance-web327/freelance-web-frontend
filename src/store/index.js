065ffe65a8f97e3e5739dc5d840dfabe7ecb4d96
Fahim Islam <127311493+cRED-f@users.noreply.github.com>
2024-05-23 15:57:57 +0000

    admin panel functionality added

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
+import rootReducer from "../features/reducers/adminReducer";
+
+/**
+ * Configures the Redux store with various reducers.
+ *
+ * @property {Object} reducer - An object containing slice reducers.
+ * @property {Function} reducer.auth - Handles authentication state.
+ * @property {Function} reducer.admin - Handles admin panel state.
+ *
+ * @returns {EnhancedStore} The configured Redux store.
+ */
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    admin: rootReducer,
   },
 });
 
